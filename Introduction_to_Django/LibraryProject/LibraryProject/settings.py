997b2ef665fa30c74830ccc36106adc028778680
Yuddie <sinclairokoro@gmail.com>
2025-02-21 16:40:26 +0100

    admin interface'

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'bookshelf.apps.BookshelfConfig'
 ]
 
 ROOT_URLCONF = 'LibraryProject.urls'
