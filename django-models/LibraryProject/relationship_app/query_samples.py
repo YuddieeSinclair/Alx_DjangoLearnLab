e62dd0ca8cb987bc5cdb33164c124cea86382670
Yuddie <sinclairokoro@gmail.com>
2025-03-05 18:16:06 +0100

    django deep dive

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,6 +1,9 @@
 from relationship_app.models import *
 
 #Query all books by a specific author
+def query_all_books(author_name):
+    my_book = Book.objects.filter(author=author_name)
+    return my_book
 
 
 
