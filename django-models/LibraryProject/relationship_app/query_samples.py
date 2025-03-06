3ac5e398b89ccde6b2661eaf2ae8a2207498c3ac
Yuddie <sinclairokoro@gmail.com>
2025-03-06 12:07:17 +0100

    django deep dive

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -13,3 +13,6 @@ def find_library(library_name):
     return my_library.books.all()
 
 #retrieve the librarian for a library
+def librarian_for_a_library(library_name):
+    my_library = Library.objects.get(name=library_name)
+    return my_library.librarian
