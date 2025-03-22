e811139dc7a0808f60334d7e22a0b01b2fee3fbe
Yuddie <sinclairokoro@gmail.com>
2025-03-22 07:27:39 +0100

    implementing RBAC

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from .views import list_books, register, LibraryDetailView
 from django.contrib.auth.views import LogoutView, LoginView
+from admin_view import admin_dashboard
 
 
 urlpatterns = [
@@ -10,4 +11,5 @@ urlpatterns = [
     path("register/", views.register, name= "register"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name= 'relationship_app/logout.html'), name= 'logout'),
+    path('isadmin/', admin_dashboard, name="admin")
 ]
