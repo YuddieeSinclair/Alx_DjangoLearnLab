b8085c7c671f740658212c16f2eedd3d01996741
Yuddie <sinclairokoro@gmail.com>
2025-03-24 15:20:29 +0100

    Implementing RBAC

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.views.generic.detail import DetailView
 from django.http import HttpResponse
 from .models import Library, UserProfile, Book
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test, login_required
 
 
 def list_books(request):
@@ -30,6 +30,7 @@ def register(request):
 def is_admin(user):
     return user.userprofile.role == 'Admin'
 
+@login_required
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, "relationship_app/admin_view.html")
