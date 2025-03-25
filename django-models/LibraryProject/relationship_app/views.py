88b019461c01751ef7b3578a63050b672e36c827
Yuddie <sinclairokoro@gmail.com>
2025-03-25 15:05:49 +0100

    custom permissions in django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.views.generic.detail import DetailView
 from django.http import HttpResponse
 from .models import Library, UserProfile, Book
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test, login_required, permission_required
+from django.contrib.auth.decorators import user_passes_test, login_required
 
 
 def list_books(request):
@@ -54,6 +54,9 @@ def is_member(user):
 @user_passes_test(is_admin)
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
+
+
+from django.contrib.auth.decorators import permission_required
 
 
 @permission_required('relationship_app.can_add_book', raise_exception=True)
