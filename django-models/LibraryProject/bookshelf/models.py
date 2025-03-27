3e7115855735f6f9069335360bcb5e2528932798
Yuddie <sinclairokoro@gmail.com>
2025-03-27 13:49:47 +0100

    advanced security

--- django-models/LibraryProject/bookshelf/models.py
+++ django-models/LibraryProject/bookshelf/models.py
@@ -8,3 +8,37 @@ class Book(models.Model):
     
     def __str__(self):
         return f"{self.title} by {self.author}, publication_year: {self.publication_year}"
+
+
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(max_length=200)
+    profile_photo = models.ImageField
+
+
+    def __str__(self):
+        return f"{CustomUser.date_of_birth}"
+    
+
+class UserManager(BaseUserManager):
+    def create_user(self, date_of_birth, profile_photo, passowrd=None):
+        user = self.model(
+            date_of_birth = date_of_birth,
+            profile_photo = profile_photo
+        )
+
+        user.set_password = passowrd
+        user.save(using=self._db)
+        return user
+    
+
+    def create_superuser(self, date_of_birth, profile_photo, passowrd=None):
+        user = self.create_user(
+            date_of_birth = date_of_birth,
+            profile_photo = profile_photo
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
