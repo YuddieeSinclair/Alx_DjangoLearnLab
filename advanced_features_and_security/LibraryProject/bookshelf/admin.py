32e9e544635ef1bd4cc64cb7df1a7d924237017c
Yuddie <sinclairokoro@gmail.com>
2025-03-28 14:36:02 +0100

    advanced sec

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -8,11 +8,11 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_year',)
 
 
-class ModelAdmin(BaseAdmin):
+class CustomModelAdmin(BaseAdmin):
     list_display = ('date_of_birth', 'profile_photo')
     search_fields = ('date_of_birth')
     list_filter = ("is_admin")
 
 # Register your models here.
 admin.site.register(Book, BookAdmin)
-admin.site.register(CustomUser, ModelAdmin)
+admin.site.register(CustomUser, CustomModelAdmin)
